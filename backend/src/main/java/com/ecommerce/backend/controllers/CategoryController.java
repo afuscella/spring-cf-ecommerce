072d63a3a2e45403201d744792f9d4ad57d915af
Arthur Silva <fuscellaarthur@gmail.com>
2021-05-31 21:57:31 -0300

    feat(service): apply CategoryDTO into service layer

--- backend/src/main/java/com/ecommerce/backend/controllers/CategoryController.java
+++ backend/src/main/java/com/ecommerce/backend/controllers/CategoryController.java
@@ -1,14 +1,12 @@
 package com.ecommerce.backend.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ecommerce.backend.entities.Category;
+import com.ecommerce.backend.models.response.CategoryResponse;
 import com.ecommerce.backend.services.CategoryServices;
 
 @RestController
@@ -19,9 +17,9 @@ public class CategoryController {
 	CategoryServices categoryServices;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> categories = categoryServices.handleIndexAll();
-		return ResponseEntity.ok().body(categories);
+	public ResponseEntity<CategoryResponse> findAll() {
+		CategoryResponse response = categoryServices.handleIndexAll();
+		return ResponseEntity.ok().body(response);
 	}
 
 }
--- backend/src/main/java/com/ecommerce/backend/services/CategoryServices.java
+++ backend/src/main/java/com/ecommerce/backend/services/CategoryServices.java
@@ -1,11 +1,15 @@
 package com.ecommerce.backend.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ecommerce.backend.entities.Category;
+import com.ecommerce.backend.models.dto.CategoryDTO;
+import com.ecommerce.backend.models.response.CategoryResponse;
 import com.ecommerce.backend.repositories.CategoryRepository;
 
 @Service
@@ -14,8 +18,12 @@ public class CategoryServices {
 	@Autowired
 	CategoryRepository categoryRepository;
 
-	public List<Category> handleIndexAll() {
-		return categoryRepository.findAll();
+	@Transactional(readOnly = true)
+	public CategoryResponse handleIndexAll() {
+		List<Category> categories = categoryRepository.findAll();
+		List<CategoryDTO> response = categories.stream().map(CategoryDTO::new).collect(Collectors.toList());
+		return new CategoryResponse(response);
+
 	}
 
 }
