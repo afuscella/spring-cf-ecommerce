229e7761841ad49b108ff9bf6223eb8c86c77ca9
Arthur Silva <fuscellaarthur@gmail.com>
2021-05-31 21:55:53 -0300

    feat(data): add initial sql data

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_category (uuid, name, created_at, updated_at) VALUES ('7776802e-55c6-44d1-99c1-95045c9681be', 'Books', now(), now());
+INSERT INTO tb_category (uuid, name, created_at, updated_at) VALUES ('8e0748a4-7c61-48aa-91dd-e358da0fb81a', 'Electronics', now(), now());
+INSERT INTO tb_category (uuid, name, created_at, updated_at) VALUES ('3d9f2f45-4c63-4daa-8285-292d5625d5a6', 'Computers', now(), now());
