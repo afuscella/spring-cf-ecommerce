e4b55459287d02455e7042a284fc15b406411e74
Arthur Silva <fuscellaarthur@gmail.com>
2021-06-02 20:59:21 -0300

    feat(dto): add ProductDTO with their entities in constructor

--- backend/src/main/java/com/ecommerce/backend/models/dto/ProductDTO.java
+++ backend/src/main/java/com/ecommerce/backend/models/dto/ProductDTO.java
@@ -1,5 +1,57 @@
 package com.ecommerce.backend.models.dto;
 
-public class ProductDTO {
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import com.ecommerce.backend.entities.Category;
+import com.ecommerce.backend.entities.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductDTO implements Serializable {
+
+	private UUID uuid;
+
+	private String name;
+
+	private String description;
+
+	private BigDecimal price;
+
+	private String imgUrl;
+
+	private Instant date;
+
+	private List<CategoryDTO> categories;
+
+	private Instant createdAt;
+
+	private Instant updatedAt;
+
+	public ProductDTO(Product product) {
+		this.uuid = product.getUuid();
+		this.name = product.getName();
+		this.description = product.getDescription();
+		this.price = product.getPrice();
+		this.imgUrl = product.getImgUrl();
+		this.date = product.getDate();
+		this.createdAt = product.getCreatedAt();
+		this.updatedAt = product.getUpdatedAt();
+	}
+
+	public ProductDTO(Product product, Set<Category> categories) {
+		this(product);
+		categories.forEach(category -> this.categories.add(new CategoryDTO(category)));
+	}
 
 }
