603075d5d1b7b54ed57cad5ebb2b5301b8c12b11
Arthur Silva <fuscellaarthur@gmail.com>
2021-06-02 22:32:24 -0300

    feat(endpoint): add get /products endpoint

--- backend/src/main/java/com/ecommerce/backend/models/response/ProductResponse.java
+++ backend/src/main/java/com/ecommerce/backend/models/response/ProductResponse.java
@@ -1,2 +1,15 @@
-package com.ecommerce.backend.models.response;public class ProductResponse {
+package com.ecommerce.backend.models.response;
+
+import org.springframework.data.domain.Page;
+
+import com.ecommerce.backend.models.dto.ProductDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ProductResponse {
+
+	private final Page<ProductDTO> data;
+
 }
