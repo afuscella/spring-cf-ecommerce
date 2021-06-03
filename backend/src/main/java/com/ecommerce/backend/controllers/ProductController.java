11b991a275657103691996bf08f02a3585ff9656
Arthur Silva <fuscellaarthur@gmail.com>
2021-06-03 00:22:43 -0300

    feat(endpoint): add /post products endpoint

--- backend/src/main/java/com/ecommerce/backend/controllers/ProductController.java
+++ backend/src/main/java/com/ecommerce/backend/controllers/ProductController.java
@@ -1,16 +1,22 @@
 package com.ecommerce.backend.controllers;
 
+import java.net.URI;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ecommerce.backend.models.dto.ProductDTO;
 import com.ecommerce.backend.models.response.ProductResponse;
@@ -43,11 +49,39 @@ public class ProductController {
 	}
 
 	@GetMapping(value = "/{uuid}")
-	public ResponseEntity<ProductDTO> findByUuid(
+	public ResponseEntity<ProductDTO> findByIndex(
 			@PathVariable
 					UUID uuid) {
 		ProductDTO productDTO = productServices.handleIndex(uuid);
 		return ResponseEntity.ok().body(productDTO);
+	}
+
+	@PostMapping
+	public ResponseEntity<ProductDTO> create(
+			@RequestBody
+					ProductDTO productDTO) {
+		productDTO = productServices.handleCreate(productDTO);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{uuid}").buildAndExpand(productDTO.getUuid()).toUri();
+		return ResponseEntity.created(uri).body(productDTO);
+	}
+
+	@PutMapping(value = "/{uuid}")
+	public ResponseEntity<ProductDTO> updateByIndex(
+			@PathVariable
+					UUID uuid,
+			@RequestBody
+					ProductDTO productDTO) {
+		productDTO = productServices.handleUpdateByIndex(uuid, productDTO);
+		return ResponseEntity.ok().body(productDTO);
+	}
+
+	@DeleteMapping(value = "/{uuid}")
+	public ResponseEntity<Void> deleteByIndex(
+			@PathVariable
+					UUID uuid) {
+		productServices.handleDeleteByIndex(uuid);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- backend/src/main/java/com/ecommerce/backend/models/dto/TransformProductDTO.java
+++ backend/src/main/java/com/ecommerce/backend/models/dto/TransformProductDTO.java
@@ -1,5 +1,39 @@
 package com.ecommerce.backend.models.dto;
 
-public final class TransformProduct {
-	public toProductEntity
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.ecommerce.backend.entities.Category;
+import com.ecommerce.backend.entities.Product;
+import com.ecommerce.backend.repositories.CategoryRepository;
+
+@Component
+public class TransformProductDTO {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	public Product toEntity(ProductDTO productDTO) {
+		Product product = new Product();
+		return mapProduct(productDTO, product);
+	}
+
+	public Product toEntity(ProductDTO productDTO, Product entityRef) {
+		return mapProduct(productDTO, entityRef);
+	}
+
+	private Product mapProduct(ProductDTO productDTO, Product product) {
+		product.setName(productDTO.getName());
+		product.setDescription(productDTO.getDescription());
+		product.setPrice(productDTO.getPrice());
+		product.setImgUrl(productDTO.getImgUrl());
+
+		product.getCategories().clear();
+		for (CategoryDTO categoryDTO : productDTO.getCategories()) {
+			Category category = categoryRepository.getOne(categoryDTO.getUuid());
+			product.getCategories().add(category);
+		}
+		return product;
+	}
+
 }
