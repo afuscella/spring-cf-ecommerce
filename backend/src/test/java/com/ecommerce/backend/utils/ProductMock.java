ca2c612028d80d613b3ae457b1fe3cc382f618be
Arthur Silva <fuscellaarthur@gmail.com>
2021-06-07 21:49:21 -0300

    fix(deps): rearrange dependencies along app

---
+++ backend/src/test/java/com/ecommerce/backend/utils/ProductMock.java
@@ -0,0 +1,25 @@
+package com.ecommerce.backend.utils;
+
+import java.math.BigDecimal;
+
+import com.ecommerce.backend.entities.Product;
+import com.ecommerce.backend.models.dto.ProductDTO;
+
+public class ProductFactory {
+
+	public static Product create() {
+		Product product = new Product();
+		product.setName("Product Test");
+		product.setDescription("Product Test Description");
+		product.setPrice(new BigDecimal("10.00"));
+		product.setImgUrl("https://via.placeholder.com/100.png");
+		product.getCategories().add(CategoryFactory.create());
+		return product;
+	}
+
+	public static ProductDTO createDTO() {
+		Product product = create();
+		return new ProductDTO(product, product.getCategories());
+	}
+
+}
